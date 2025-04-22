f44a5bab04b650dbbb053c427e0fc9dabe9ebd7f
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2025-04-22 23:19:38 +0700

    Revert "add eslint and prettier"
    This reverts commit f279e4ef8a6d850f6605a95def0f7bfd0f9bc448.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import sum from './sum';
-
 const express = require('express');
 const app = express();
 
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const sum = require('../sum');
+=======
+const sum = require('../src/sum');
+>>>>>>> parent of f279e4e (add eslint and prettier)
 
 test('should add two numbers correctly', () => {
   const result = sum(2, 2);
